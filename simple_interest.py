4905dc1d7d7b5f265912ce3b4088304cbc8e4f78
manikant <mk8847932@gmail.com>
2025-11-29 20:24:51 +0530

    simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+import sys
+if len(sys.argv)! =4:
+  print("usage:python simple_interest.py <principal> <rate> <time>")
+  sys.exit(1)
+  P = (sys.argv[1])
+  R = (sys.argv[2])
+  T = (sys.argv[3])
+
+s1 - (P * R * T) / 100
+print("simple interest:, s1)
